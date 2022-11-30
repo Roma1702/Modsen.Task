740f7f9e034337fdbce5bb4508c7ee8fe46a7165
Roma <romabelka43@gmail.com>
2022-11-30 23:54:41 +0300

    entites initial implementation

---
+++ Entities/Identity/User.cs
@@ -0,0 +1,10 @@
+﻿using Entities.Implementation;
+using Microsoft.AspNetCore.Identity;
+
+namespace Entities.Identity;
+
+public class User : IdentityUser<Guid>
+{
+    public int Age { get; set; }
+    public virtual List<UserConnectionWithEvent>? ConnectionWithEvents { get; set; }
+}
---
+++ Entities/Implementation/UserConnectionWithEvent.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Identity;
+
+namespace Entities.Implementation
+{
+    public class UserConnectionWithEvent
+    {
+        public Guid Id { get; set; }
+        public Guid EventId { get; set; }
+        public virtual Event? Event { get; set; }
+        public Guid UserId { get; set; }
+        public virtual User? User { get; set; }
+    }
+}

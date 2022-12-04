bbd6c932df04df1372d56180fdf5fda4afac25db
Roma <romabelka43@gmail.com>
2022-12-05 00:44:03 +0300

    models add changes

---
+++ Models/Core/EventMemberModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Models.Core
+{
+    public class EventMemberModel
+    {
+        public Guid Id { get; set; }
+        public Guid UserId{ get; set; }
+        public Guid EventId{ get; set; }
+        public Guid RoleId{ get; set; }
+    }
+}
